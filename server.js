fc744189607fa4635a7fd33106f238e123fcf9fe
Fabian Loaeza <floaeza@bbinco.com>
2022-12-01 11:46:30 -0700

    Cambio database

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-// app.use((err, req, res, next) => {
-//   err.statusCode = err.statusCode || 500;
-//   err.message = err.message || "Internal Server Error";
-//   res.status(err.statusCode).json({
-//     message: err.message,
-//   });
-// });
+app.get('/', (req, res) => {
+  res.send('Servidor Funcionando!')
+})
 routerApi(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
